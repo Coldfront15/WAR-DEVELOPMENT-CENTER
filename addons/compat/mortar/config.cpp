a7da0442bde817015c276199aada5fe198d7f014
Coldfront15 <born2toot2@gmail.com>
2026-09-05 19:28:03 -0700

    ~ Fixed : Mortar Auto Fire on Load

--- addons/compat/mortar/config.cpp
+++ addons/compat/mortar/config.cpp
@@ -6,7 +6,12 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-		requiredAddons[] = {"cba_main"};
+		requiredAddons[] = {
+            "cba_main",
+            "ace_interact_menu",
+            "ww2_spe_assets_c_vehicles_staticweapons_u1_c",
+            "WW2_SPEX_Core_c_ace_compat_spex_c_csw"
+            };
 		skipWhenMissingDependencies = 1;
 		author = "Coldfront15";
         VERSION_CONFIG;
